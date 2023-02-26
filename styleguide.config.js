48494f7522d1a7897b5899d9bd31417f32ef4db6
Craig McClanahan <craigmcc@gmail.com>
2023-02-26 15:56:29 -0800

    Flesh out documentation for the remaining button components.

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,9 +1,44 @@
 module.exports = {
     components: "src/**/[A-Z]*.tsx",
     ignore: ["**/*.test.tsx", "src/test/*.tsx"],
+    pagePerSection: true,
     propsParser: require("react-docgen-typescript").withDefaultConfig({
         savePropValueAsString: true,
     }).parse,
+    sections: [
+        {
+            components: [
+                "src/**/AddButton/AddButton.tsx",
+                "src/**/BackButton/BackButton.tsx",
+                "src/**/EditButton/EditButton.tsx",
+                "src/**/ForwardButton/ForwardButton.tsx",
+                "src/**/TextButton/TextButton.tsx",
+            ],
+            description: "Individual buttons with icon contents.",
+            name: "Buttons",
+        },
+        {
+            components: [
+                "src/**/CheckBoxField/CheckBoxField.tsx",
+                "src/**/SelectField/SelectField.tsx",
+                "src/**/TextField/TextField.tsx",
+            ],
+            description:  "Components for form fields using react-hook-form.",
+            name: "Form Fields",
+        },
+        {
+            components: [
+                "src/**/Callout/Callout.tsx",
+                "src/**/CheckBox/CheckBox.tsx",
+                "src/**/FetchingProgress.tsx",
+                "src/**/MutatingProgress.tsx",
+                "src/**/Pagination.tsx",
+                "src/**/SearchBar.tsx",
+            ],
+            description: "General purpose stand alone components.",
+            name: "Other",
+        },
+    ],
     styleguideDir: "docs",
     template: {
         head: {
