fe5e55dcf031f62986f74a1a3cd5864d4f437c43
Craig McClanahan <craigmcc@gmail.com>
2023-12-08 10:49:30 -0800

    Rename setupTests.js to setupTests.ts and require to import.

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
     preset: 'ts-jest',
-    setupFilesAfterEnv: ['./src/setupTests.js'],
+    setupFilesAfterEnv: ['./src/setupTests.ts'],
     testEnvironment: 'jsdom',
 };
